17740e901e873eac5d61149dcd9ffb328ed20902
ddoongmause <DDoongMause@gmail.com>
2021-03-10 22:26:41 +0900

    벨로퍼트 Next.js 연습

---
+++ pages/ssr-test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Layout from "../components/Layout";
+import axios from "axios";
+
+
+class SSRTest extends React.Component{
+    static async getInitialProps ({req}){
+        //return req ? {from: 'server'} : {from: 'client'}
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        
+        return {
+            users: response.data
+        }
+    }
+
+    render() {
+        const {users} = this.props;
+
+        const userList = users.map(
+            user => <li key={user.id}>{user.username}</li>
+        )
+
+        return (
+            <Layout>
+                <ul>
+                    {userList}
+                </ul>
+            </Layout>
+        );
+    }
+}
+
+export default SSRTest;
